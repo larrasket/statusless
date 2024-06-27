e4c3a763c512498807ca45eed5951aec7012438f
Salih Muhammed <lr0@gmx.com>
2024-06-27 13:42:48 +0300

    Fix systemd issue

--- plugins/local_ip_warp.go
+++ plugins/local_ip_warp.go
@@ -2,6 +2,7 @@ package plugins
 
 import (
 	"os/exec"
+	"strings"
 	"time"
 )
 
@@ -35,7 +36,7 @@ func isServiceActive(serviceName string) (bool, error) {
 	// Execute the systemctl command to check the service status
 	cmd := exec.Command("systemctl", "is-active", serviceName)
 	output, err := cmd.Output()
-	if err != nil {
+	if err != nil && !strings.Contains(string(output), "inactive") {
 		return false, err
 	}
 
